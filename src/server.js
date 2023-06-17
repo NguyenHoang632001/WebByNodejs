b37496b930c55c6f3f96c061054030c271ea8de6
NguyenHoang632001 <nguyenhoang632001@gmail.com>
2023-06-17 17:20:16 +0700

    config viewEngine

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import viewEngine from "./configs/viewEngine";
 const app = express();
 const port = 3000;
+viewEngine(app);
 app.get("/welcome", (req, res) => {
-  res.send("Hello World!");
+  res.render("index.ejs");
 });
 app.listen(port, () => {
   console.log("Running in port 3000");
