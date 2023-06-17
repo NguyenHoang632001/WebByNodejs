b88d7fb3c170d77e745dd0626be726cc0de024f2
NguyenHoang632001 <nguyenhoang632001@gmail.com>
2023-06-17 18:27:11 +0700

    config connectDB Sequelize

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ import express from "express";
 import viewEngine from "./configs/viewEngine";
 import initWebRoute from "./route/web";
 import dotenv from "dotenv";
+import connectDB from "./configs/connectDB";
 dotenv.config();
 const app = express();
 
 let port = process.env.PORT || 3001;
 viewEngine(app);
 initWebRoute(app);
-
+connectDB();
 app.listen(port, () => {
   console.log("Running in port", port);
 });
