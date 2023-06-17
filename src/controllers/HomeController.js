3faa9f30fa0c6bd27996c423c7cd74abf8641f3e
NguyenHoang632001 <nguyenhoang632001@gmail.com>
2023-06-17 18:15:15 +0700

    config controllers

---
+++ src/controllers/HomeController.js
@@ -0,0 +1,6 @@
+const getHomePage = (req, res) => {
+  res.render("HomePage.ejs");
+};
+module.exports = {
+  getHomePage,
+};
--- src/route/web.js
+++ src/route/web.js
@@ -1,9 +1,8 @@
 import express from "express";
+import HomeControllers from "../controllers/HomeController";
 const router = express.Router();
 const initWebRoute = (app) => {
-  router.get("/", (req, res) => {
-    res.send("Hi router");
-  });
+  router.get("/home", HomeControllers.getHomePage);
   return app.use("/", router);
 };
 module.exports = initWebRoute;
